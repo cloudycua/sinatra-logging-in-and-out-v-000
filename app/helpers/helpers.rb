a8804e4c366916073bac981651601ed27fcec270
Christine Cua <cloudycua@gmail.com>
2018-07-25 21:03:56 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,9 @@
 class Helpers
 
+# finds user in database that matches in inputed username and password
   def current_user
-
+    
+      @user = User.find_by(username: params[:username])
   end
 
   def is_logged_in?
