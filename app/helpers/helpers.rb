86810fb7c3a4bea1628a76aa29024feededc5896
Christine Cua <cloudycua@gmail.com>
2018-07-25 21:08:34 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,13 +1,13 @@
+require 'sinatra/base'
+
 class Helpers
 
-# finds user in database that matches in inputed username and password
-  def current_user
-      @user = User.find_by(username: params[:username])
-      @user
+  def self.current_user(session)
+    @user = User.find_by_id(session[:user_id])
   end
 
-  def is_logged_in?
-
+  def self.is_logged_in?(session)
+    !!session[:user_id]
   end
 
 end
