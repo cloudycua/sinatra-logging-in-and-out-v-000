9989ed2561537bdfee94e821eed35b756bf5149a
Christine Cua <cloudycua@gmail.com>
2018-07-25 20:52:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,9 @@ class ApplicationController < Sinatra::Base
     @user = User.find_by(username: params[:username])
   # sets session user id to database user id
     session[:user_id] = @user.id
-  # displays correct username and balance based on session  
+  # displays correct username and balance based on session
   # displays a 'Log Out' link
+    erb :account
   end
 
   get '/account' do
