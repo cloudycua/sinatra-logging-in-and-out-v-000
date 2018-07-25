85ef8ffc15a7eb9985b24736bd426cfa0f4610d0
Christine Cua <cloudycua@gmail.com>
2018-07-25 20:49:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-# sets session user id to id of user
+# sets session user id to id of user from database
   post '/login' do
     @user = User.find_by(username: params[:username])
     session[:user_id] = @user.id
